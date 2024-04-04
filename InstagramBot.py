55bdeb8f982a2eb59ab7dadd0c7fd7ba999dcbab
Rayan Darazi <53384481+Rayn2005@users.noreply.github.com>
2024-04-05 00:56:03 +1300

    Updated code 2
    get_following() now writes the dictionary of following into a seprate file. Also added a new function get_unfollowing(), however the runtime of this funtion is very long at the moment

--- InstagramBot.py
+++ InstagramBot.py
@@ -3,6 +3,8 @@ import os
 from time import sleep
 import time
 import datetime
+import pickle
+import math
 
 from selenium.webdriver import Keys
 from selenium.webdriver.support.ui import WebDriverWait
@@ -50,10 +52,14 @@ class InstagramBot:
     def get_following(self):
         self.driver.get(f"https://www.instagram.com/{self.username}/following/")
         time.sleep(5)
-        element = "." + ".".join("_ap3a _aaco _aacw _aacx _aad7 _aade".split())
-        followers_list = self.driver.find_element(By.CSS_SELECTOR, "._aano")
 
-        for i in range(20):
+        followers_list = self.driver.find_element(By.CSS_SELECTOR, "._aano")
+        following_number_elements = self.driver.find_elements(By.CSS_SELECTOR, self.get_css("html-span xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x1hl2dhg x16tdsg8 x1vvkbs"))
+
+        following_number = int(following_number_elements[2].get_attribute("innerHTML"))
+        time_to_scroll = math.ceil((following_number / 4) * 1.1)
+
+        for i in range(int(time_to_scroll)):
             followers_list.send_keys(Keys.SPACE)
             time.sleep(0.5)
 
@@ -70,12 +76,32 @@ class InstagramBot:
                 following_list.append(following_element)
             
 
-
+        #initialises the following dictionary
         for follower in following_list:
             follower_name = follower.get_attribute("innerHTML")
             follower_link = f"https://www.instagram.com/{follower_name}/"
             if follower_name not in follower_dict:
                 follower_dict[follower_name] = follower_link
+
+        #Stores dictionary of following in seperate file 
+        dict_file = open("following_dict.pkl", "wb")
+        pickle.dump(follower_dict, dict_file)
+        dict_file.close()
+
+
+
+
+    def get_unfollowing(self, following_dict):
+        unfollowing_list = []
+        for following_name, following_link in following_dict.items():
+            self.driver.get(f"{following_link}following/")
+
+            search_bar = self.webDriverWait(self.driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, self.get_css('x1lugfcp x19g9edo x1lq5wgf xgqcy7u x30kzoy x9jhf4c x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x5n08af x5yr21d x1a2a7pz xyqdw3p x1pi30zi xg8j3zb x1swvt13 x1yc453h xh8yej3 xhtitgo xs3hnx8 xoy4bel x7xwk5j xvs91rp xp001vz'))))
+            search_bar.send_keys(self.username)
+            time.sleep(1)
+            
+            no_result = self.driver.find_elements(By.CSS_SELECTOR, self.get_css("x1lliihq x1plvlek xryxfnj x1n2onr6 x193iq5w xeuugli x1fj9vlw x13faqbe x1vvkbs x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x x1i0vuye xvs91rp xo1l8bm x1roi4f4 x1tu3fi x3x7a5m x10wh9bi x1wdrske x8viiok x18hxmgj"))
+            if len(no_result) != 0:
+                unfollowing_list.append(following_name)
         
-        print(follower_dict)
-        print(len(follower_dict))
+        return unfollowing_list
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 from time import sleep
 import time
 import datetime
+import pickle
 from InstagramBot import InstagramBot
 
 from selenium.webdriver import Keys
@@ -10,16 +11,33 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC  # Helps us check for the presence of an element
 from selenium.webdriver.common.by import By  # defines by what we are searching
 
-"""
 username = input(print("Username: "))
 username = username.strip()
 password = input(print("Password: "))
 password = password.strip()
-"""
-ig = InstagramBot("rdtester21", "cranking90s") # an instagram account created for the sole purpose of testing this
+
+
+
+
+ig = InstagramBot(username, password)
+
+
 time.sleep(1)  # use sleep to wait for a page to load before running scripts on the page
 ig.login()
 time.sleep(5)
 ig.get_following()
+
+dict_file = open("following_dict.pkl", "rb")
+following_dict = pickle.load(dict_file)
+dict_file.close()
+print(len(following_dict))
+
+print(ig.get_unfollowing(following_dict))
+
+
+
+    
+
+
 
 
